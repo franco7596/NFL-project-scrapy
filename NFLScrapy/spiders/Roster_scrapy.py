a583e32908ea7519f79db54d4224f55107319c23
franco7596 <franco_7_5_96@hotmail.com>
2023-02-17 00:09:54 -0300

    fixes of spider roster

--- NFLScrapy/spiders/Roster_scrapy.py
+++ NFLScrapy/spiders/Roster_scrapy.py
@@ -22,14 +22,36 @@ class Roster_NFL_spider(scrapy.Spider):
 
     def parses_roster(self, response,  **kwargs):
         nameTeam = kwargs['nameTeam']
-        playersLink = response.xpath('//a[@class="nfl-o-roster__player-name nfl-o-cta--link"]/@href').getall()
+        cantPlayers = response.xpath('//tbody/tr').getall()
+        playersLink = []
+        for i in range(len(cantPlayers)):
+            playersLink.append( response.xpath(f'//tbody/tr[{i+1}]//a[@class="nfl-o-roster__player-name nfl-o-cta--link"]/@href').get() or "")
         for i in range(len(playersLink)):
-            playerName = response.xpath(f'(//a[@class="nfl-o-roster__player-name nfl-o-cta--link"]/text())[{i+1}]').get() or ''
-            playerNumber = response.xpath(f'(//tr/td[2]/text())[{i+1}]').get() or ''
-            playerPosition = response.xpath(f'(//tr/td[3]/text())[{i+1}]').get() or ''
-            playerStatus = response.xpath(f'(//tr/td[4]/text())[{i+1}]').get() or ''
-            playerImage = response.xpath(f'(//td//img[@class="img-responsive"]/@src)[{i+1}]').get() or None
-            yield response.follow(playersLink[i], callback=self.parses_player, cb_kwargs= {'playerName':playerName , 'playerNumber':playerNumber, 'playerPosition':playerPosition, 'playerStatus':playerStatus,'playerImage':playerImage, 'nameTeam':nameTeam})
+            playerName = response.xpath(f'//tbody/tr[{i+1}]//a[@class="nfl-o-roster__player-name nfl-o-cta--link"]/text()').get() or response.xpath(f'//tbody/tr[{i+1}]//span/text()').get()
+            playerNumber = response.xpath(f'//tbody/tr[{i+1}]/td[2]/text()').get() or ''
+            playerPosition = response.xpath(f'//tbody/tr[{i+1}]/td[3]/text()').get() or ''
+            playerStatus = response.xpath(f'//tbody/tr[{i+1}]/td[4]/text()').get() or ''
+            playerImage = response.xpath(f'//tbody/tr[{i+1}]//img[@class="img-responsive"]/@src').get() or ''
+            if(playersLink[i] != "" ):
+                yield response.follow(playersLink[i], callback=self.parses_player, cb_kwargs= {'playerName':playerName , 'playerNumber':playerNumber, 'playerPosition':playerPosition, 'playerStatus':playerStatus,'playerImage':playerImage, 'nameTeam':nameTeam})
+            else:
+                player = ScrapyPlayer()
+                player['nameTeam'] = nameTeam
+                player['name'] = playerName
+                player['image'] = playerImage
+                player['status'] = playerStatus
+                player['number'] = playerNumber
+                player['position'] = playerPosition
+                player['height'] = ""
+                player['age'] = ""
+                player['experience'] = ""
+                player['college'] = ""
+                player['weight'] = ""
+                player['arms'] = ""
+                player['hands'] = ""
+                player['hometown'] = ""
+                player['timestamp'] = datetime.today().strftime('%Y-%m-%d')
+                yield player
 
 
     def parses_player(self, response, **kwargs):
@@ -40,7 +62,7 @@ class Roster_NFL_spider(scrapy.Spider):
         playerStatus = kwargs['playerStatus']
         playerImageLazy = kwargs['playerImage']
         playerImage = ""
-        if(playerImageLazy):
+        if(playerImageLazy != ''):
             playerImage = playerImageLazy.replace('/t_lazy','')
         playerHeight = response.xpath('//div[@class="nfl-c-player-info__key" and contains(.,"Height")]/following-sibling::div/text()').get() or ''
         playerWeight = response.xpath('//div[@class="nfl-c-player-info__key" and contains(.,"Weight")]/following-sibling::div/text()').get() or ''
@@ -66,5 +88,35 @@ class Roster_NFL_spider(scrapy.Spider):
         player['arms'] = playerArms
         player['hands'] = playerHands
         player['hometown'] = playerHometown
+        player['timestamp'] = datetime.today().strftime('%Y-%m-%d')
+        yield player
+
+
+    def parses_player_without_link(self, kwargs):
+        nameTeam = kwargs['nameTeam']
+        playerName = kwargs['playerName']
+        playerNumber = kwargs['playerNumber']
+        playerPosition = kwargs['playerPosition']
+        playerStatus = kwargs['playerStatus']
+        playerImageLazy = kwargs['playerImage']
+        playerImage = ""
+        if(playerImageLazy != ''):
+            playerImage = playerImageLazy.replace('/t_lazy','')
+
+        player = ScrapyPlayer()
+        player['nameTeam'] = nameTeam
+        player['name'] = playerName
+        player['image'] = playerImage
+        player['status'] = playerStatus
+        player['number'] = playerNumber
+        player['position'] = playerPosition
+        player['height'] = ""
+        player['age'] = ""
+        player['experience'] = ""
+        player['college'] = ""
+        player['weight'] = ""
+        player['arms'] = ""
+        player['hands'] = ""
+        player['hometown'] = ""
         player['timestamp'] = datetime.today().strftime('%Y-%m-%d')
         yield player
